080a1091286b2e6bceb2469f64215bc176ffbc71
emc <micha3l_255@yahoo.com>
2020-10-19 22:58:22 -0500

    Updates

---
+++ precache-manifest.429ee8f3af5c058b9fe88b49b07cb71e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a3d4a3dbe7f4eb6056e989d6aeb740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09ca1510bff5f35e86e0",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "cbff3a28a385ea810dfb",
+    "url": "/static/css/main.e544d28d.chunk.css"
+  },
+  {
+    "revision": "09ca1510bff5f35e86e0",
+    "url": "/static/js/2.39aa0dc0.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.39aa0dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbff3a28a385ea810dfb",
+    "url": "/static/js/main.84c6cb9c.chunk.js"
+  },
+  {
+    "revision": "547e2fd08b4511394dae",
+    "url": "/static/js/runtime-main.41a154bd.js"
+  },
+  {
+    "revision": "94d9b986890f26eae212ec5b70951031",
+    "url": "/static/media/history-background.94d9b986.jpg"
+  },
+  {
+    "revision": "1cfab70da1e50178763eba616e4ccb4b",
+    "url": "/static/media/homepage-background.1cfab70d.png"
+  },
+  {
+    "revision": "371f073775f397b63e4c1636ccf3ecd1",
+    "url": "/static/media/mountain-snow.371f0737.jpg"
+  },
+  {
+    "revision": "b98173fd4503f2cce0f26cc7c7b71cba",
+    "url": "/static/media/s1 copy.b98173fd.png"
+  },
+  {
+    "revision": "b98173fd4503f2cce0f26cc7c7b71cba",
+    "url": "/static/media/s1.b98173fd.png"
+  },
+  {
+    "revision": "1ddf91d410088ec2fa01158460a8327d",
+    "url": "/static/media/s2 copy.1ddf91d4.png"
+  },
+  {
+    "revision": "1ddf91d410088ec2fa01158460a8327d",
+    "url": "/static/media/s2.1ddf91d4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
