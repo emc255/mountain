ff113c1b39510e23afefdb2b619d925e7f74e862
emc <micha3l_255@yahoo.com>
2020-10-23 10:58:21 -0500

    Updates

---
+++ precache-manifest.cab787aebc9fbb2e8fb81422df5fe733.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65afe96838f9582018613e17b91d99b",
+    "url": "/mountain/index.html"
+  },
+  {
+    "revision": "61787c7c440d51f8ea35",
+    "url": "/mountain/static/css/2.044e715c.chunk.css"
+  },
+  {
+    "revision": "cbff3a28a385ea810dfb",
+    "url": "/mountain/static/css/main.e544d28d.chunk.css"
+  },
+  {
+    "revision": "61787c7c440d51f8ea35",
+    "url": "/mountain/static/js/2.cc044f2c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/mountain/static/js/2.cc044f2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbff3a28a385ea810dfb",
+    "url": "/mountain/static/js/main.84c6cb9c.chunk.js"
+  },
+  {
+    "revision": "a7b3867c65dbf0ed56dc",
+    "url": "/mountain/static/js/runtime-main.1cfa5647.js"
+  },
+  {
+    "revision": "94d9b986890f26eae212ec5b70951031",
+    "url": "/mountain/static/media/history-background.94d9b986.jpg"
+  },
+  {
+    "revision": "1cfab70da1e50178763eba616e4ccb4b",
+    "url": "/mountain/static/media/homepage-background.1cfab70d.png"
+  },
+  {
+    "revision": "371f073775f397b63e4c1636ccf3ecd1",
+    "url": "/mountain/static/media/mountain-snow.371f0737.jpg"
+  },
+  {
+    "revision": "b98173fd4503f2cce0f26cc7c7b71cba",
+    "url": "/mountain/static/media/s1 copy.b98173fd.png"
+  },
+  {
+    "revision": "b98173fd4503f2cce0f26cc7c7b71cba",
+    "url": "/mountain/static/media/s1.b98173fd.png"
+  },
+  {
+    "revision": "1ddf91d410088ec2fa01158460a8327d",
+    "url": "/mountain/static/media/s2 copy.1ddf91d4.png"
+  },
+  {
+    "revision": "1ddf91d410088ec2fa01158460a8327d",
+    "url": "/mountain/static/media/s2.1ddf91d4.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/mountain/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/mountain/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/mountain/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/mountain/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.429ee8f3af5c058b9fe88b49b07cb71e.js"
+  "/mountain/precache-manifest.cab787aebc9fbb2e8fb81422df5fe733.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mountain/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
